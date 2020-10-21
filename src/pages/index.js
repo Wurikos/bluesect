385dc0c303949a39b2568629d32f0f18024a820e
Wurikos <50768680+Wurikos@users.noreply.github.com>
2020-10-21 12:08:25 +0300

    Some changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,8 +12,8 @@ export default (() => {
 			<meta name={"description"} content={"Web site created using quarkly.io"} />
 			<link rel={"shortcut icon"} href={"https://uploads.quarkly.io/readme/cra/favicon-32x32.ico"} type={"image/x-icon"} />
 		</Helmet>
-		<Section background="--color-primary" />
 		<Section background="--color-orange" />
-		<Section background="--color-green" />
+		<Section background="--color-purple" />
+		<Section background="--color-darkL1" />
 	</Theme>;
 });
